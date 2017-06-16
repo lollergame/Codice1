4958418732c8f650db6301bec73c01e287b29b57
ciroso <nesdark@hotmail.it>
2017-06-16 20:12:01 +0200

    provando4 (Sistemato l' = e la somma)

--- ValoMatrix.h
+++ ValoMatrix.h
@@ -12,7 +12,7 @@
 class ValoMatrix : public Matrix {
 public:
 
-    ValoMatrix(int x, int y) {        //, T initvalnumb = 0){
+    ValoMatrix(const int x, const int y) {
         width = x;
         height = y;
         value = new int[width * height];
@@ -25,13 +25,13 @@ public:
         delete[] value;
     }
 
-    int getValue(int x, int y) const {
+    int getValue(const int x, const int y) const {
         if ((x >= 0) && (x < width) && (y >= 0) && (y < height))
             return value[x + y * height];
         else return 0;
     }
 
-    bool setValue(int x, int y, int val) {
+    bool setValue(const int x, const int y, const int val) {
         if ((x >= 0) && (x < width) && (y >= 0) && (y < height)) {
             value[x + y * height] = val;
             std::cout << "imposato valore " << val << " in x " << x << " y " << y << std::endl;
@@ -42,7 +42,7 @@ public:
         }
     }
 
-    void printMatrix() {
+    void printMatrix() const {
         for (int i = 0; i < width; i++) {
             std::cout << std::endl;
             for (int j = 0; j < height; j++) {
@@ -57,7 +57,7 @@ public:
 
     }
 
-    ValoMatrix prendiRiga(const int x) {
+    ValoMatrix prendiRiga(const int x) const {
         ValoMatrix temp(1, this->width);
         if (x >= 0 && x < this->width) {
             for (int i = 0; i < this->width; i++) {
@@ -68,7 +68,7 @@ public:
         } else return temp;
     }
 
-    ValoMatrix prendiColonna(int y) {
+    ValoMatrix prendiColonna(const int y) const {
         ValoMatrix temp(this->height, 1);
         if (y >= 0 && y < this->height) {
             for (int i = 0; i < this->height; i++) {
@@ -78,7 +78,7 @@ public:
         } else return temp;
     }
 
-    ValoMatrix prod(ValoMatrix &rh) {
+    ValoMatrix prod(const ValoMatrix &rh) {
         ValoMatrix temp(width, rh.height);
         if (this->height == rh.width) {
             for (int i = 0; i < this->width; i++) {
@@ -110,17 +110,6 @@ public:
     ValoMatrix operator=(const ValoMatrix &rh) const {
         ValoMatrix temp(rh.getWidth(), rh.getHeight());
         if (this != &rh) {
-            /*this->width = rh.width;
-            this->height = rh.height;
-            if (this->value)
-                delete[] value;
-            for (int i = 0; i < this->width; i++)
-                for (int j = 0; j < this->height; j++) {
-                    this->setValue(i, j, rh.getValue(i,j));
-                }
-            return *this;
-        } else return *this;*/
-            //E' una valida alternativa...
             temp.width = rh.width;
             temp.height = rh.height;
             if (this->value)

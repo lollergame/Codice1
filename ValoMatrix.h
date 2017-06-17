c04a04f1ff322217e3bd48a0378c015ec94db4ca
ciroso <nesdark@hotmail.it>
2017-06-17 17:10:24 +0200

    provando 7 (aggiunta delle eccezioni e pulizia generale dei commenti)

--- ValoMatrix.h
+++ ValoMatrix.h
@@ -5,7 +5,9 @@
 #ifndef EXAMDIPRO_VALOMATRIX_H
 #define EXAMDIPRO_VALOMATRIX_H
 
+#include <stdexcept>
 #include "Matrix.h"
+
 //TODO unit testing urgente
 
 //template<typename T>
@@ -21,25 +23,25 @@ public:
                 value[i + j * height] = 0;
     }
 
-    ~ValoMatrix() {
+    virtual ~ValoMatrix() {
         delete[] value;
     }
 
-    int const getValue(const int x, const int y) const {
+    int const getValue(const int x, const int y) const throw(std::out_of_range) {
         if ((x >= 0) && (x < width) && (y >= 0) && (y < height))
             return value[x + y * height];
-        else return 0;
+        else
+            throw (std::out_of_range(
+                    "Out Of Range per la selezione degli indici di riga e/o di colonna, La Matrice ha dimensioni diverse"));
     }
 
-    bool setValue(const int x, const int y, const int val) {
+    void setValue(const int x, const int y, const int val) throw(std::out_of_range) {
         if ((x >= 0) && (x < width) && (y >= 0) && (y < height)) {
             value[x + y * height] = val;
             std::cout << "imposato valore " << val << " in x " << x << " y " << y << std::endl;
-            return true;
-        } else {
-            std::cout << "inserimento non riuscito" << std::endl;
-            return false;
-        }
+        } else
+            throw (std::out_of_range(
+                    "Out Of Range per la selezione degli indici di riga e/o di colonna, la Matrice ha dimensioni diverse"));
     }
 
     void printMatrix() const {
@@ -54,31 +56,29 @@ public:
             }
         }
         std::cout << std::endl << std::endl;
-
     }
 
-    ValoMatrix prendiRiga(const int x) const {
+    ValoMatrix prendiRiga(const int x) const throw(std::out_of_range) {
         ValoMatrix temp(1, this->width);
         if (x >= 0 && x < this->width) {
             for (int i = 0; i < this->width; i++) {
                 temp.setValue(0, i, this->getValue(x, i));
             }
             return temp;
-        } else return temp;
+        } else throw (std::out_of_range("Out Of Range con il valore x"));
     }
 
-    ValoMatrix prendiColonna(const int y) const {
+    ValoMatrix prendiColonna(const int y) const throw(std::out_of_range) {
         ValoMatrix temp(this->height, 1);
         if (y >= 0 && y < this->height) {
             for (int i = 0; i < this->height; i++) {
                 temp.setValue(i, 0, this->getValue(i, y));
             }
             return temp;
-        } else return temp;
+        } else throw (std::out_of_range("Out Of Range con il valore y"));
     }
 
-    ValoMatrix &trasp() {
-        //ValoMatrix temp(this->width,this->height);
+    ValoMatrix &trasp() throw(std::out_of_range) {
         if (this->value) {
             for (int i = 0; i < this->width; i++)
                 for (int j = i; j < this->height; j++) {
@@ -87,14 +87,23 @@ public:
                     this->setValue(j, i, tempor);
                 }
             return *this;
-        } else {
-            ValoMatrix *zero = new ValoMatrix(1, 1);
-            return *zero;
-        }
-        //else throw
+        } else throw (std::out_of_range("La Matrice non contiene valori"));
     }
 
-    ValoMatrix operator*(const ValoMatrix &rh) {
+    ValoMatrix &underScale(const int x, const int y) throw(std::out_of_range) {
+        if ((this->width > 1) && (this->width > 1)) {
+            if (x < this->width && y < this->height) {
+                this->width = x;
+                this->height = y;
+                for (int i = x; i < this->width; ++i)
+                    for (int j = y; j < this->height; ++j)
+                        this->setValue(i, j, 0);
+                return *this;
+            } else throw (std::out_of_range("Fuori Range"));
+        } else throw (std::out_of_range("La Matrice e' gia alla dimensione minima! Impossibile scalare di dimensione"));
+    }
+
+    ValoMatrix operator*(const ValoMatrix &rh) throw(std::out_of_range) {
         ValoMatrix temp(this->width, rh.height);
         if (this->height == rh.width) {
             for (int i = 0; i < this->width; i++) {
@@ -107,11 +116,12 @@ public:
                 }
             }
             return temp;
-        }
-        return temp;
+        } else
+            throw (std::out_of_range(
+                    "La Matrice non e' conformabile! La dimensione delle righe della prima Matrice e delle colonne della seconda devono essere uguali!!"));
     }
 
-    ValoMatrix operator+(const ValoMatrix &rh) {
+    ValoMatrix operator+(const ValoMatrix &rh) const throw(std::out_of_range) {
         ValoMatrix temp(rh.width, rh.height);
         if ((this->width == rh.width) && (this->height == rh.height)) {
             for (int i = 0; i < this->width; i++)
@@ -119,25 +129,20 @@ public:
                     temp.setValue(i, j, (this->getValue(i, j) + rh.getValue(i, j)));
                 }
             return temp;
-        } else return temp;
+        } else
+            throw (std::out_of_range(
+                    "La Matrice non e' conformabile!! La dimensione delle righe e delle colonne delle due Matrici deve esere uguale!! "));
     }
 
     ValoMatrix &operator=(const ValoMatrix &rh) {
-        //ValoMatrix* temp = new ValoMatrix(rh.getWidth(), rh.getHeight());
         if (this != &rh) {
             this->width = rh.width;
             this->height = rh.height;
-            //if (this->value)
-            //  delete[] value;
             for (int i = 0; i < this->width; i++)
-                for (int j = 0; j < this->height; j++) {
+                for (int j = 0; j < this->height; j++)
                     this->setValue(i, j, rh.getValue(i, j));
-                }
             return *this;
-        } else {
-            ValoMatrix *zero = new ValoMatrix(1, 1);
-            return *zero;
-        }
+        } else return *this;
     }
 
     int getWidth() const {
--- main.cpp
+++ main.cpp
@@ -5,64 +5,75 @@
 using namespace std;
 
 int main() {
+    try {
+        cout << "Istanzio A come 2x2" << endl;
+        ValoMatrix A(2, 2);
+        int tre = 3;
+        int quattro = 4;
+        A.setValue(0, 0, 1);
+        A.setValue(0, 1, 2);
+        A.setValue(1, 0, tre);
+        A.setValue(1, 1, quattro);
+        A.printMatrix();
 
-    cout<<"Istanzio A come 2x2"<<endl;
-    ValoMatrix A(2, 2);
-    int tre = 3;
-    int quattro = 4;
-    A.setValue(0, 0, 1);
-    A.setValue(0, 1, 2);
-    A.setValue(1, 0, tre);
-    A.setValue(1, 1, quattro);
-    A.printMatrix();
+        cout << endl << "Istanzio B come 2x2" << endl;
+        ValoMatrix B(2, 2);
+        B.setValue(0, 0, 5);
+        B.setValue(0, 1, 6);
+        B.setValue(1, 0, 7);
+        B.setValue(1, 1, 8);
+        B.printMatrix();
 
-    cout<<endl<<"Istanzio B come 2x2" << endl;
-    ValoMatrix B(2, 2);
-    B.setValue(0, 0, 5);
-    B.setValue(0, 1, 6);
-    B.setValue(1, 0, 7);
-    B.setValue(1, 1, 8);
-    B.printMatrix();
+        cout << endl << "Istanzio C come A + B" << endl;
+        ValoMatrix C = A + B;
+        C.printMatrix();
 
-    cout <<endl<< "Istanzio C come A + B" << endl;
-    ValoMatrix C = A + B;
-    C.printMatrix();
+        cout << endl << endl << "Istanzio D come prima riga di C " << endl;
+        ValoMatrix D = C.prendiRiga(0);
+        D.printMatrix();
 
-    cout<<endl<<endl<<"Istanzio D come prima riga di C "<<endl;
-    ValoMatrix D = C.prendiRiga(0);
-    D.printMatrix();
+        cout << endl << "Istanzio E come seconda riga di C " << endl;
+        ValoMatrix E = C.prendiRiga(1);
+        E.printMatrix();
 
-    cout<<endl<<"Istanzio E come seconda riga di C "<<endl;
-    ValoMatrix E = C.prendiRiga(1);
-    E.printMatrix();
+        cout << endl << "Istanzio F come prima colonna di C" << endl;
+        ValoMatrix F = C.prendiColonna(0);
+        F.printMatrix();
 
-    cout<<endl<< "Istanzio F come prima colonna di C"<<endl;
-    ValoMatrix F = C.prendiColonna(0);
-    F.printMatrix();
+        cout << endl << "Istanzio G come seconda colonna di C" << endl;
+        ValoMatrix G(C.getWidth(), C.getHeight());
+        G = C.prendiColonna(1);
+        G.printMatrix();
 
-    cout<<endl<< "Istanzio G come seconda colonna di C"<<endl;
-    ValoMatrix G(C.getWidth(), C.getHeight());
-    G = C.prendiColonna(1);
-    G.printMatrix();
+        cout << "Istanzio H come prodotto tra A e B" << endl;
+        ValoMatrix H = A * B;
+        H.printMatrix();
 
-    cout << "Istanzio H come prodotto tra A e B" << endl;
-    ValoMatrix H = A * B;
-    H.printMatrix();
+        cout << "Istanzio I come copia di A e traspongo" << endl;
+        ValoMatrix I(2, 2);
+        cout << "Copio A in I" << endl;
+        I = A;
+        cout << "Traspongo I" << endl;
+        I.trasp();
+        cout << "Printo I" << endl;
+        I.printMatrix();
 
-    cout << "Istanzio I come copia di A e traspongo" << endl;
-    ValoMatrix I(2, 2);
-    cout << "Copio A in I" << endl;
-    I = A;
-    cout << "Traspongo I" << endl;
-    I.trasp();
-    cout << "Printo I" << endl;
-    I.printMatrix();
+        cout << "A" << endl;
+        A.printMatrix();
+        cout << "traspongo A" << endl;
+        A.trasp();
+        A.printMatrix();
 
-    cout << "A" << endl;
-    A.printMatrix();
-    cout << "traspongo A" << endl;
-    A.trasp();
-    A.printMatrix();
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+        cout << "Orlata 1x1 di A" << endl;
+        A.underScale(1, 1);
+        A.printMatrix();
+
+        cout << "Orlata 10x10 di A" << endl;
+        A.underScale(10, 10);
+        A.printMatrix();
+
+        std::cout << "Hello, World!" << std::endl;
+        return 0;
+    }
+    catch (std::out_of_range &e) { cerr << e.what() << endl; }
 }
